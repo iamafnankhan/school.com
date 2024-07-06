28e1dbfbc59f9c4be5adfe59c7e3138db894dd31
afnan khan <afnanweb1122@gmail.com>
2024-07-06 14:33:45 +0500

    admin route url clear

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -182,7 +182,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="{{url ('/dashboard')}}" class="nav-link">
+            <a href="{{url ('/admin/dashboard')}}" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
 
               <p>
